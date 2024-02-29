f101ab3e091c002881d390ee679ce321b4cabd58
hongshulin1994 <2576469175@qq.com>
2024-03-01 00:30:26 +0800

    test

--- demo-activiti/src/test/java/com/xkcoding/activiti/SpringBootDemoActivitiApplicationTests.java
+++ demo-activiti/src/test/java/com/xkcoding/activiti/SpringBootDemoActivitiApplicationTests.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootDemoActivitiApplicationTests {
-
+//
     @Autowired
     private ProcessRuntime processRuntime;
     @Autowired
